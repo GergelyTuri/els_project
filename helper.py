dbc9b1b61f51c60bc70198331329455794d6a04a
gergely <gt2253@cumc.columbia.edu>
2023-02-03 11:01:44 -0500

    adding readme file

--- helper.py
+++ helper.py
@@ -14,3 +14,20 @@ def auth():
     creds, _ = default()
     gc = gspread.authorize(creds)
     return
+
+def load_spreadsheet_data(spreadsheet, sheet):
+  """
+    Loads data from a specified sheet in a given Google Spreadsheet.
+
+    Parameters:
+    spreadsheet (str): The name of the Google Spreadsheet.
+    sheet (str): The name of the sheet in the Spreadsheet.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the data from the specified sheet.
+    """
+  workbook = gc.open(spreadsheet)
+  values = workbook.worksheet(sheet).get_all_values()
+  df = pd.DataFrame.from_records(values[:]).drop(0).reset_index(drop=True)
+  df.columns = values[0]
+  return df
