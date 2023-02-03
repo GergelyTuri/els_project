4bf7abc588405c7b739e77b5367da1cdcf0cf23d
gergely <gt2253@cumc.columbia.edu>
2023-02-03 10:49:58 -0500

    adding readme file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='els_project',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/GergelyTuri/els_project',
+    license='MIT',
+    author='Gergely Turi',
+    author_email='gt2253@cumc.columbia.edu',
+    description='base code for analyzing ELS data'
+)
