f8e6ac144cfda722e0841dcb7a04e3c9d611d58a
NovakHChen <hc3429@columbia.edu>
2025-02-11 10:55:01 -0500

    plots for freezing anova with 1s bout length

--- src/visualization.py
+++ src/visualization.py
@@ -6,30 +6,35 @@ Visualization Functions for plotting Freezing data, Moseq data, and other data
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
 
-def plot_freezing_time(sefla_data, subset_data, effect_size, pvalue, title_text, hue, ylim=[0, 60], output_filename='Freezing Time RM-ANOVA.svg'):
+def plot_freezing_time(data, variable, effect_size, pvalue, title_text, hue, ylim=[0, 60], output_filename='Freezing Time RM-ANOVA.svg'):
     """
     Plots the preferred subsets of freezing time data for sefla stage and other stages side by side using a double axis plot. 
 
     Parameters:
-    sefla_data (pd.DataFrame): A DataFrame containing the sefla stage data.
-    subset_data (pd.DataFrame): A DataFrame containing the data for the other stages. (designed used for data including seflb stage to recall 4 stage)
+    data (pd.DataFrame): A DataFrame containing the freezing time data with a 'day' column and 'freezing' column.
     effect_size (float): The effect size of the difference between the two groups of data using repeated-measure ANOVA. 
     pvalue (float): The p-value of the difference between the two groups of data using repeated-measure ANOVA.
     title_text (str): The title text to be displayed on the plot specifying the data being compared.
+    hue (str): The column name used for grouping data (e.g., treatment group).
     ylim (list): The y-axis limits for the plot. Default is [0, 60].
-    output_filename (str): The filename to save the plot as. Default is 'figure_2a_freezing_cond.svg'.
+    output_filename (str): The filename to save the plot as. Default is 'Freezing Time RM-ANOVA.svg'.
 
     Returns:
     A double axis plot comparing the freezing time data between groups for the sefla stage and other stages side by side.
     """
+    # Subset the data
+    sefla_data = data[data['day'] == 'sefla']
+    subset_data = data[data['day'].isin(['seflb', 'recall1', 'recall2', 'recall3', 'recall4'])]
+
+    plt.rcParams['font.family'] = 'Arial'  # Choose a vector-safe font
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 6), sharey=True, gridspec_kw={'width_ratios': [1, 4]})
 
     # Plot the sefla data on the first axis
-    sns.pointplot(ax=ax1, data=sefla_data, x='day', y='freezing', hue=hue, join=True)
+    sns.pointplot(ax=ax1, data=sefla_data, x='day', y=variable, hue=hue, join=True)
     ax1.set_ylabel('Freezing Time (sec)')
     ax1.set_ylim(ylim)
     ax1.set_xlabel('')
@@ -37,14 +42,14 @@ def plot_freezing_time(sefla_data, subset_data, effect_size, pvalue, title_text,
     sns.despine(ax=ax1)
 
     # Plot the non-sefla data on the second axis
-    sns.pointplot(ax=ax2, data=subset_data, x='day', y='freezing', hue=hue, join=True)
+    sns.pointplot(ax=ax2, data=subset_data, x='day', y=variable, hue=hue, join=True)
     ax2.set_xlabel('')
     ax2.set_ylabel('Freezing Time (sec)')
     ax2.legend(title=hue)
     sns.despine(ax=ax2)
 
     # Add main title
-    fig.suptitle(f'Freezing Time by {title_text}', fontsize=16, x=0.5)
+    fig.suptitle(f'{title_text}', fontsize=16, x=0.5)
     fig.text(0.5, -0.05, 'Experimental Stage', ha='center', fontsize=12)
 
     # Determine comparison sign for p-value
